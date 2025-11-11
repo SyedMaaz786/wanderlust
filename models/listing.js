8fa1c5e55f54fd5d52be9c4eced2f2e42c1e5eb9
Syed Maaz <syedmaaz78622@gmail.com>
2025-11-11 13:15:56 +0530

    I have updated the listing.js file and resolved the error where the image if we add a new listing in my website it was not getting displayed now that is being displayed

--- models/listing.js
+++ models/listing.js
@@ -2,25 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      default: 'https://images.unsplash.com/photo-1613977257363-707ba9348227?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bHV4dXJ5JTIwdmlsbGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+
+
+      set: (v) =>
+        v === ""
+          ? 'https://images.unsplash.com/photo-1613977257363-707ba9348227?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bHV4dXJ5JTIwdmlsbGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60'
+
+
+          : v,
     },
-    description: String,
-    image: {
-        filename: {
-            type: String,
-        },
-        url: {
-            type: String,
-            default: 'https://unsplash.com/photos/a-man-standing-on-top-of-a-lush-green-hillside-h1q2CM-at0Q',
-          
-            set: (v) => v === "" ? 'https://unsplash.com/photos/a-man-standing-on-top-of-a-lush-green-hillside-h1q2CM-at0Q' : v,
-        }
-    },
-    price: Number,
-    location: String,
-    country: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model('Listing', listingSchema);
